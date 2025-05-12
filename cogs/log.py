da97c44aeb7af08b5953964b458756535965caf7
r.panja codes <101450382+rpanjacodes@users.noreply.github.com>
2025-05-12 15:06:34 +0530

    Update log.py

--- cogs/log.py
+++ cogs/log.py
@@ -1,20 +1,3 @@
-# M.I.T.A - Discord Bot Project
-# Copyright (C) 2025 M.I.T.A Bot Team
-# 
-# This file is part of M.I.T.A.
-# 
-# M.I.T.A is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# M.I.T.A is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <https://www.gnu.org/licenses/>.
 import discord
 from discord.ext import commands
 from discord import app_commands
@@ -24,7 +7,6 @@ class Logs(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # Member banned log
     @commands.Cog.listener()
     async def on_member_ban(self, guild, user):
         if is_log_enabled(guild.id):
@@ -42,7 +24,23 @@ class Logs(commands.Cog):
                 except discord.Forbidden:
                     pass
 
-    # Member left/kicked log
+    @commands.Cog.listener()
+    async def on_member_unban(self, guild, user):
+        if is_log_enabled(guild.id):
+            channel_id = get_log_channel_id(guild.id)
+            channel = guild.get_channel(channel_id)
+            if channel:
+                embed = discord.Embed(
+                    title="♻️ Member Unbanned",
+                    description=f"**{user}**",
+                    color=discord.Color.green()
+                )
+                embed.set_footer(text=f"User ID: {user.id}")
+                try:
+                    await channel.send(embed=embed)
+                except discord.Forbidden:
+                    pass
+
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         if is_log_enabled(member.guild.id):
@@ -60,7 +58,6 @@ class Logs(commands.Cog):
                 except discord.Forbidden:
                     pass
 
-    # Set log channel
     @app_commands.command(name="set_log_channel", description="Set the channel where logs will be posted.")
     @app_commands.checks.has_permissions(manage_guild=True)
     async def set_log_channel(self, interaction: discord.Interaction, channel: discord.TextChannel):
@@ -77,7 +74,6 @@ class Logs(commands.Cog):
         else:
             await interaction.response.send_message(f"⚠️ Error: `{error}`", ephemeral=True)
 
-    # Toggle logging
     @app_commands.command(name="toggle_logs", description="Enable or disable log messages.")
     @app_commands.checks.has_permissions(manage_guild=True)
     async def toggle_logs(self, interaction: discord.Interaction, enabled: bool):
